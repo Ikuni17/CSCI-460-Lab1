4f44b20f996b9c79e02bf7d4f0f90cbb56ce407e
BradWhite <white.brad17@gmail.com>
2017-09-26 13:35:17 -0600

    Added Simpy example

--- white_bradley - 1.py
+++ white_bradley - 1.py
@@ -1,2 +1,47 @@
-# Python 3.5.1
-# 1876 % 3 = 1 + 2 = 3 processors
+'''
+Bradley White
+Programming 1: Multi-Processor Management
+CSCI-460: Operating Systems
+September 26, 2017
+'''
+
+# Interpreter: Python 3.5.1
+# k = 1876 % 3 + 2 = 3 processors
+
+# $pip install simpy
+import simpy
+
+
+class Processor:
+    def __init__(self, id):
+        self.id = id
+
+# Simpy example follows
+class Car(object):
+    def __init__(self, env):
+        self.env = env
+        # Start the run process everytime an instance is created.
+        self.action = env.process(self.run())
+
+    def run(self):
+        while True:
+            print('Start parking and charging at %d' % self.env.now)
+            charge_duration = 5
+            # We yield the process that process() returns
+            # to wait for it to finish
+            yield self.env.process(self.charge(charge_duration))
+            # The charge process has finished and
+            # we can start driving again.
+            print('Start driving at %d' % self.env.now)
+            trip_duration = 2
+            yield self.env.timeout(trip_duration)
+
+    def charge(self, duration):
+        yield self.env.timeout(duration)
+
+def main():
+    env = simpy.Environment()
+    car = Car(env)
+    env.run(until=15)
+
+main()

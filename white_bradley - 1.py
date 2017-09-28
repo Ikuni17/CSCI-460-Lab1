cb0740cd5a4e9ebf35ed5104b2de57e2c4edcab8
BradWhite <white.brad17@gmail.com>
2017-09-28 08:50:41 -0600

    Fixed generator bug

--- white_bradley - 1.py
+++ white_bradley - 1.py
@@ -18,48 +18,22 @@ jobs = [(4, 9), (15, 2), (18, 16), (20, 3), (26, 29), (29, 198), (35, 7), (45, 1
 random_jobs = []
 
 
-class Processor:
-    def __init__(self, id):
-        self.id = id
-
-
-# Simpy example follows
-class Car(object):
-    def __init__(self, env):
-        self.env = env
-        # Start the run process everytime an instance is created.
-        self.action = env.process(self.run())
-
-    def run(self):
-        while True:
-            print('Start parking and charging at %d' % self.env.now)
-            charge_duration = 5
-            # We yield the process that process() returns
-            # to wait for it to finish
-            yield self.env.process(self.charge(charge_duration))
-            # The charge process has finished and
-            # we can start driving again.
-            print('Start driving at %d' % self.env.now)
-            trip_duration = 2
-            yield self.env.timeout(trip_duration)
-
-    def charge(self, duration):
-        yield self.env.timeout(duration)
-
-
-class Job_scheduler:
+class Job_scheduler(object):
     def __init__(self, env, num_processors):
         self.env = env
         self.processor = simpy.Resource(env, num_processors)
 
-    def run_job(self, time):
+    def run_job(self, name, time):
+        print("Running job {0} with runtime {1}".format(name, time))
         yield self.env.timeout(time)
 
-def job(env, job, job_scheduler, job_num):
+
+def job(env, job_name, job_time, job_scheduler):
     with job_scheduler.processor.request() as request:
         yield request
-        print("Starting job #{0}".format(job_num))
-        yield env.process(job_scheduler.run_job(job[1]))
+        # print("Starting job #{0}".format(job_num))
+        yield env.process(job_scheduler.run_job(job_name, job_time))
+
 
 def setup_sim(env, num_processors):
     global jobs
@@ -67,7 +41,22 @@ def setup_sim(env, num_processors):
     job_scheduler = Job_scheduler(env, num_processors)
 
     for i in range(len(jobs)):
-        env.process(job(env, jobs[i], job_scheduler, i))
+        yield env.timeout(1)
+        env.process(job(env, 'job %d' % (i + 1), jobs[i][1], job_scheduler))
+
+    '''i = 1
+    while True:
+        yield env.timeout(1)
+        i+=1
+        env.process(job1(env, 'job %d' % i, 1, job_scheduler))'''
+
+
+def job_gen():
+    global jobs
+
+    for job in jobs:
+        yield job
+
 
 def create_random_jobs():
     global random_jobs
@@ -84,12 +73,13 @@ def main():
 
     env = simpy.Environment()
     env.process(setup_sim(env, k))
-    env.run
+    env.run()
     '''car = Car(env)
     env.run(until=15)
     car2 = Car(env)
     env.run(until=30)'''
-    #create_random_jobs()
-    #print(random_jobs[1], random_jobs[2])
+    # create_random_jobs()
+    # print(random_jobs[1], random_jobs[2])
+
 
 main()
